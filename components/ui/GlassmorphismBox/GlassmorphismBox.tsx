fc7633def106a4877b0d75f6eac43dc1e316a9be
Hayoung <gkduddy0814@gmail.com>
2022-02-13 01:16:23 +0900

    feat : add responsive GlassComponent and add size

--- components/ui/GlassmorphismBox/GlassmorphismBox.tsx
+++ components/ui/GlassmorphismBox/GlassmorphismBox.tsx
@@ -8,13 +8,25 @@ interface GlassmorphismBoxProps {
   className?: string;
   children?: any;
   title?: string;
+  size?: 'l' | 'm' | 's';
 }
 
 const GlassmorphismBox = (props: GlassmorphismBoxProps) => {
-  const { className, children, title } = props;
+  const { className, children, title, size = 'l' } = props;
   return (
-    <div className={cn(className, s.root)}>
-      {title && <h2 className={s.title}>{title}</h2>}
+    <div
+      className={cn(
+        className,
+        s.root,
+        `${size === 'l' && s.large}`,
+        `${size === 'm' && s.medium}`,
+        `${size === 's' && s.small}`,
+      )}>
+      {title && (
+        <h2 className={s.title}>
+          <b>{title}</b>
+        </h2>
+      )}
       <div>{children}</div>
     </div>
   );
