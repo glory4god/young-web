77c30d0ebc87f6db2818821edd0e2907b8f2583f
Hayoung <gkduddy0814@gmail.com>
2022-02-13 17:00:37 +0900

    refactor :

---
+++ utils/constants/index.ts
@@ -0,0 +1,21 @@
+export const MY_INFO = [
+  { title: '이름', content: '유하영' },
+  { title: 'MBTI', content: 'ESTP' },
+  { title: '키', content: '178.6 cm' },
+  { title: '몸무게', content: '69 kg' },
+  { title: '취미', content: '커피마시기<br />카페가기' },
+  {
+    title: '관심분야?',
+    content: '최근들어 UI 인터랙션!?!<br />백오피스 개발?',
+  },
+  {
+    title: '관심기술?',
+    content: 'SWR / CSS / responsive web / typescript generic',
+  },
+  {
+    title: '2022 목표',
+    content:
+      '운동 다시 시작하기..<br />수학 강의 영상 찍기!!<br />코테 공부(js)',
+  },
+  { title: '경력', content: '1년도 안된.. 주니어' },
+];
